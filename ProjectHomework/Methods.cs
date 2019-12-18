acef20cf58d30b6f6cc198d623864df0878ae8d6
PavelEagle <47183588+PavelEagle@users.noreply.github.com>
2019-12-18 10:17:22 +0300

    Add method TicTacToeCheck
    Add method TicTacToeCheck

--- ProjectHomework/Methods.cs
+++ ProjectHomework/Methods.cs
@@ -6,7 +6,7 @@ namespace ProjectHomework
 {
     class Methods
     {
-        public static int[] ArrRandomCreating(int massiveSize)
+        public int[] ArrRandomCreating(int massiveSize)
         {
             Random rnd = new Random();
             int[] arr = new int[massiveSize];
@@ -17,7 +17,7 @@ namespace ProjectHomework
             return arr;
         }
 
-        public static void arrViewer1D(int[] arr)
+        public void arrViewer1D(int[] arr)
         {
 
             for (int i = 0; i < arr.Length; i++)
@@ -26,5 +26,102 @@ namespace ProjectHomework
             }
             Console.WriteLine();
         }
+        
+        public int TicTacToeCheck(int[] arr)
+        {
+
+            int markersCount = 0;
+
+            if (row - 1 >= 0 && field[row - 1, column] == currentMarker)
+            {
+                markersCount = 2;
+
+                if (row - 2 >= 0 && field[row - 2, column] == currentMarker)
+                {
+                    markersCount = 3;
+                }
+                else if (row + 1 < 5 && field[row + 1, column] == currentMarker)
+                {
+                    markersCount = 3;
+                }
+
+            }
+
+            if (row - 1 >= 0 && column + 1 < 5 && field[row - 1, column + 1] == currentMarker)
+            {
+                markersCount = 2;
+
+                if (row - 2 >= 0 && column + 2 < 5 && field[row - 2, column + 2] == currentMarker)
+                {
+                    markersCount = 3;
+                }
+                else if (row + 1 < 5 && column - 1 >= 0 && field[row + 1, column] == currentMarker)
+                {
+                    markersCount = 3;
+                }
+
+            }
+
+            if (column + 1 >= 5 && field[row, column + 1] == currentMarker)
+            {
+                markersCount = 2;
+
+                if (column + 2 >= 5 && field[row, column + 2] == currentMarker)
+                {
+                    markersCount = 3;
+                }
+                else if (column - 1 >= 0 && field[row, column - 1] == currentMarker)
+                {
+                    markersCount = 3;
+                }
+
+            }
+
+            if (row + 1 < 5 && column + 1 < 5 && field[row + 1, column + 1] == currentMarker)
+            {
+                markersCount = 2;
+
+                if (row + 2 < 5 && column + 2 < 5 && field[row + 2, column + 2] == currentMarker)
+                {
+                    markersCount = 3;
+                }
+                else if (row - 1 >= 0 && column - 1 >= 0 && field[row - 1, column - 1] == currentMarker)
+                {
+                    markersCount = 3;
+                }
+
+            }
+
+            if (row + 1 >= 5 && field[row + 1, column] == currentMarker)
+            {
+                markersCount = 2;
+
+                if (row + 2 >= 5 && field[row + 2, column] == currentMarker)
+                {
+                    markersCount = 3;
+                }
+                else if (row - 1 >= 0 && field[row - 1, column] == currentMarker)
+                {
+                    markersCount = 3;
+                }
+
+            }
+
+            if (row + 1 < 5 && column - 1 >= 0 && field[row + 1, column - 1] == currentMarker)
+            {
+                markersCount = 2;
+
+                if (row + 2 < 5 && column - 2 >= 0 && field[row + 2, column - 2] == currentMarker)
+                {
+                    markersCount = 3;
+                }
+                else if (row - 1 >= 0 && column + 1 >= 0 && field[row - 1, column + 1] == currentMarker)
+                {
+                    markersCount = 3;
+                }
+            }
+            return markersCount;
+        }
+        
     }
 }
