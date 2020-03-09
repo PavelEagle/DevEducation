a7b864415c289eaa3723fc7978a02d362d580499
Pavel <pavel.eagle1@gmail.com>
2020-03-09 11:36:24 +0300

    Remove unused comment

--- ProjectHomework/Program.cs
+++ ProjectHomework/Program.cs
@@ -6,24 +6,7 @@ namespace ProjectHomework
     {
         static void Main(string[] args)
         {
-            //HomeWork1 hw1 = new HomeWork1();
-            //HomeWork2 hw2 = new HomeWork2();
-            //HomeWork3 hw3 = new HomeWork3();
-
-            //LinkedList llist = new LinkedList();
-
-            //llist.Add(new LinkedList.Node(3));
-            //llist.Add(new LinkedList.Node(0));
-            //llist.Add(new LinkedList.Node(-1));
-            //llist.Add(new LinkedList.Node(4));
-            //llist.AddAllAtIndex(2,new int[] { 1,4 , 4, 4, 3, 4 });
-            //llist.PrintList();
-            //llist.RemoveAll(4);
-            //llist.PrintList();
-
-
-            //llist.Reverse();
-            //llist.PrintList();
+ 
             double[,] arr = { { 5, 4, -2, 1, 11 }, { 2, -1, 4, 0, 12 }, { 3, 2, 0, 0, 7 }, { 4, 0, 0, 0, 4 } };
 
             Gauss gs = new Gauss(arr);
@@ -33,98 +16,10 @@ namespace ProjectHomework
             gs.printResult();
 
         }
-        //int[] newArr = arrList.RemoveVal(1);
-        //arrList.PrintArr(newArr);
-        //Console.WriteLine();
-        //newArr = arrList.RemoveVal(1);
-        //arrList.PrintArr(newArr);
-        //Console.WriteLine();
-        //newArr = arrList.RemoveVal(1);
-        //arrList.PrintArr(newArr);
-        //Console.WriteLine();
-        //newArr = arrList.RemoveVal(1);
-        //arrList.PrintArr(newArr);
-
-
-        //Console.WriteLine($"{newArr[0]},{newArr[1]}, {newArr[2]}");
 
         int[] arrOfNumbers = { 11, 23, 54, 68, 93, 35, 79, 55 };
-
-            //Console.WriteLine(hw1.CalcHypotenuse(54,76));
-
-            //Console.WriteLine(hw1.CalcCreditPayments(5000,5,0.15)[0]);
-
-            //Console.WriteLine(hw1.EquationOfLine(1, 1, 2, 2)[1]);
-
-            //Console.WriteLine(hw1.MultiplyAndSumDigitsOfNumber(1230)[0]);
-
-            // Console.WriteLine(hw1.CalcQuadraticQquation(2,8,3)[1]);
-
-            //Console.WriteLine(hw1.CalcExpression(-4,8,6));
-
-            //double[] arr = hw1.SolutionsOfEquation(5,15,2);
-
-            //for (int i=0; i<arr.Length; i++)
-            //{
-            //    Console.Write(arr[i]+" ");
-            //}
-
-            //Console.WriteLine(hw1.CalcQuadraticQquation(1,8,-3)[1]);
-
-            //Console.WriteLine(hw2.Calculator(96, 30, "/"));
-
-            //Console.WriteLine(hw2.SumBetweenMinMax(new int[] { -14, 16, 3, 0, 21, 9 }));
-
-            //Console.WriteLine(hw3.CountOfDividers(7, 77, 9));
-
-            //Console.WriteLine(hw3.TicTacToe(4));
-
-            //Human ant = new Human("Ant");
-            //ant.GetInfo();
-            //Human vas = new Human(2);
-            //vas.GetInfo();
-            //Human andr = new Human("Andrej", 33);
-            //andr.GetInfo();
-
-            //Human kirill = new Human() { name = "Kir", age = 22 };
-            //kirill.GetInfo();
-
-
 
         }
     }
 
     
- 
-
-
-    //int result = 0;
-    //Console.WriteLine("Введите первое число:");
-    //            try
-    //            {
-    //                number1 = Convert.ToInt32(Console.ReadLine());
-    //            }
-    //            catch
-    //            {
-    //                Console.WriteLine("Неверно задано значение первого числа");
-    //                continue;
-    //            }
-
-    //            Console.WriteLine("Введите второе число:");
-    //            try
-    //            {
-    //                number2 = Convert.ToInt32(Console.ReadLine());
-    //            }
-    //            catch
-    //            {
-    //                Console.WriteLine("Неверно задано значение второго числа");
-    //                continue;
-    //            }
-
-    //            Console.WriteLine("Введите операцию: ");
-    //            Console.WriteLine(" '-'- разность: ");
-    //            Console.WriteLine("'+' - сумма: ");
-    //            Console.WriteLine("'*' - умножение: ");
-    //            Console.WriteLine("'/' - деление: ");
-    //            Console.WriteLine("'0' - выход: ");
-
