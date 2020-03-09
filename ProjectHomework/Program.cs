e6bd7fa6cfdce574108ab30abfdc9c216db81048
Pavel <pavel.eagle1@gmail.com>
2020-03-10 00:34:24 +0300

    Add doublyLinkedList

--- ProjectHomework/Program.cs
+++ ProjectHomework/Program.cs
@@ -9,11 +9,23 @@ namespace ProjectHomework
  
             double[,] arr = { { 5, 4, -2, 1, 11 }, { 2, -1, 4, 0, 12 }, { 3, 2, 0, 0, 7 }, { 4, 0, 0, 0, 4 } };
 
-            Gauss gs = new Gauss(arr);
+            //Gauss gs = new Gauss(arr);
 
-            gs.PrintArr();
-            gs.SolutionOfGaussEquation();
-            gs.printResult();
+            //gs.PrintArr();
+            //gs.SolutionOfGaussEquation();
+            //gs.printResult();
+
+            DoublyLinkedList dl = new DoublyLinkedList();
+            dl.AddFirst(1);
+            dl.AddLast(3);
+            dl.AddLast(5);
+            dl.AddLast(7);
+            dl.AddLast(9);
+
+            dl.PrintList();
+            Console.WriteLine();
+            dl.Reverse();
+            dl.PrintList();
 
         }
 
