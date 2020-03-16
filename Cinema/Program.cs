39b83f1800900bcfedfcf7902c136cf54128db57
Pavel <pavel.eagle1@gmail.com>
2020-03-16 21:50:07 +0300

    Add TryParse on Input

--- Cinema/Program.cs
+++ Cinema/Program.cs
@@ -9,18 +9,44 @@ namespace Grafs
     {
         static void Main(string[] args)
         {
+            int countOfHall = 0, countOfFilms = 0;
             List<Film> films = new List<Film>();
-
-            Console.WriteLine("Введите количество залов в кинотеатре:");
-            int countOfHall = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Введите количество фильмов:");
-            int count = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < count; i++)
+            while (countOfHall == 0)
             {
-                Console.WriteLine($"Введите название и длительность сеанса №{i + 1} в минутах.");
-                string filmName = Console.ReadLine();
-                int filmDuration = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите количество залов в кинотеатре:");
+                while (Int32.TryParse(Console.ReadLine(), out int result))
+                {
+                    countOfHall = result;
+                    break;
+                }
+            }
+
+
+            while (countOfFilms == 0)
+            {
+                Console.WriteLine("Введите количество фильмов:");
+                while (Int32.TryParse(Console.ReadLine(), out int result))
+                {
+                    countOfFilms = result;
+                    break;
+                }
+            }
+
+            
+            for (int i = 0; i < countOfFilms; i++)
+            {
+                int filmDuration = 0;
+                string filmName="";
+                while (filmDuration == 0)
+                {
+                    Console.WriteLine($"Введите название и длительность сеанса №{i + 1} в минутах.");
+                    filmName = Console.ReadLine();
+                    while (Int32.TryParse(Console.ReadLine(), out int result))
+                    {
+                        filmDuration = result;
+                        break;
+                    }
+                }
 
                 films.Add(new Film(filmName, filmDuration));
             }
